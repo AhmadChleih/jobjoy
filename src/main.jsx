d8637288733d6c05b151158de0d87697b767abab
AhmadChleih <101448145+AhmadChleih@users.noreply.github.com>
2024-04-27 23:31:53 +0200

    Add router setup and pages to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Pages
+import Jobs from "./pages/Jobs";
+import NotFound from "./pages/NotFound";
+
+// Router Setup
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/jobs",
+    element: <Jobs />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
