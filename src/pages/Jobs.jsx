c57cb900c5e3362e8e018f13549f318e013c512a
AhmadChleih <101448145+AhmadChleih@users.noreply.github.com>
2024-04-28 14:59:44 +0200

    Refactor JobListings component to use useState and useEffect hooks for fetching and displaying job data

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,9 +1,13 @@
 import Navbar from "../components/Navbar";
+import JobListings from "../components/JobListings";
 
 const Jobs = () => {
   return (
     <>
       <Navbar />
+      <section className="bg-blu-50 px-4 py-6">
+        <JobListings />
+      </section>
     </>
   );
 };
